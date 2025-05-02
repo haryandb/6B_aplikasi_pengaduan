2e93ec86a6fa8ca70f0faa02cf9b08243c204b8a
Haryan Dwi Baharna <haryandb@gmail.com>
2025-05-02 14:14:53 +0700

    grid layout

--- resources/views/view-bootstrap.blade.php
+++ resources/views/view-bootstrap.blade.php
@@ -1,8 +1,48 @@
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
-<!-- Bootstrap JS -->
-<script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<div class="container mt-5">
-    <h2 class="text-primary">Selamat Datang!</h2>
-    <button class="btn btn-success">Tombol Bootstrap</button>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Contoh Grid Bootstrap</title>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <style>
+        .box {
+            background-color: #0d6efd;
+            color: white;
+            text-align: center;
+            padding: 20px;
+            border: 1px solid #fff;
+        }
+    </style>
+</head>
+
+<body class="container">
+
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 ">
+            <div class="box">col-md-6 col-lg-3 </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 ">
+            <div class="box">col-md-6 col-lg-3 </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 ">
+            <div class="box">col-4 col-lg-3</div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 ">
+            <div class="box">col-4 col-lg-3</div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 ">
+            <div class="box">col-4 col-lg-3</div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 ">
+            <div class="box">col-4 col-lg-3</div>
+        </div>
+    </div>
+
+</body>
+
+</html>
